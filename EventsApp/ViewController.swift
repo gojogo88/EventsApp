37087f484f94893888f7765dd6b95d28f44fa042
Jonathan Go <gojonathan.dev@gmail.com>
2022-02-16 22:46:58 +0100

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Jonathan Go on 16.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
